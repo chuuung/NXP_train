601e15b0cf9d547f97249c0a6611f0e799205e42
b0hu <b1cd34erwt@gmail.com>
2023-10-21 11:26:33 +0800

    Modify UI

--- train/views.py
+++ train/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
- 
+context = {
+    'p1' : 1,
+    'p2' : 2,
+    'p3' : 3,
+    'p4' : 15,
+}
+
 def train(request):
-    return render(request, 'train/train.html')
+    return render(request, 'train/train.html',context=context)
